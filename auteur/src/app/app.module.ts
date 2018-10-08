64a1c46b5b43021d3ce907ec9ec1be21aeb2abaa
Ciara Lynch <s00156269@mail.itsligo.ie>
2018-10-08 21:40:26 +0100

    Added initial layout to home

--- auteur/src/app/app.module.ts
+++ auteur/src/app/app.module.ts
@@ -4,14 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+   MatSidenavModule,
+   MatToolbarModule,
+   MatListModule,
+   MatIconModule,
+   MatButtonModule
+  } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule ,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
