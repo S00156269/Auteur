8a6e0fab07990a04af8c58910479d1a1c454a899
Ciara Lynch <s00156269@mail.itsligo.ie>
2019-04-28 17:17:28 +0100

    Home update & TypeScript version fix

--- auteur/src/app/home/home.component.ts
+++ auteur/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TmdbService } from 'src/shared/tmdb.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  popularFilms: Observable<any>;
+  
+  constructor(private router: Router, private route: ActivatedRoute, private tmdb: TmdbService) { }
 
   ngOnInit() {
+    this.tmdb.getPopularFilms().subscribe(search => {
+      console.log(search);
+      this.popularFilms = search.results;
+      this.popularFilms.forEach(result => {
+        result["url"] = "https://image.tmdb.org/t/p/w1280" + result["poster_path"];
+      });
+      console.log(this.popularFilms)
+    })
   }
 
+  viewFilm(id) {
+    this.router.navigate(['/film'], { queryParams: { id: id } });
+  }
 }
